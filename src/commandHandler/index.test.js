3b6289df501c46f68de22bddcc5017f3e3df48e6
Jay Freestone <mail@jayfreestone.com>
2019-02-17 21:30:23 +0000

    tests(commandHandler): Update to test result.

--- src/commandHandler/index.test.js
+++ src/commandHandler/index.test.js
@@ -1,9 +1,25 @@
+const { NORTH } = require('./../consts/orientations');
 const commandHandler = require('./index');
 
 describe('handles command', () => {
   test('move forward', () => {
-    const robotInst = { moveTo: jest.fn() };
+    const robotInst = {
+      moveTo: jest.fn(),
+      getOrientation() {
+        return NORTH;
+      },
+      getPos() {
+        return [3, 4];
+      },
+    };
+
     commandHandler('F', robotInst).execute();
-    expect(robotInst.moveTo.mock.calls.length).toBe(1);
+
+    const { calls } = robotInst.moveTo.mock;
+    expect(calls.length).toBe(1);
+    // The x position should remain the same.
+    expect(calls[0][0]).toBe(3);
+    // The y position should increment by one.
+    expect(calls[0][1]).toBe(5);
   });
 });
