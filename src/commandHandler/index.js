7e8847bfa24d032af624043a514b76102a42e8fc
Jay Freestone <mail@jayfreestone.com>
2019-02-17 21:36:42 +0000

    feat(commandHandler): Add full forward movement command.

--- src/commandHandler/index.js
+++ src/commandHandler/index.js
@@ -1,9 +1,25 @@
-function createMoveForwardCommand(actor) {
+const calculateForwardPosition = require('./../utils/calculateForwardPosition');
+
+function createMoveCommand(newPos, actor) {
+  const oldPos = actor.getPos();
   return {
     execute() {
-      actor.moveTo();
+      actor.moveTo(...newPos);
+    },
+    undo() {
+      actor.moveTo(...oldPos);
     },
   };
+}
+
+function createMoveForwardCommand(actor) {
+  return createMoveCommand(
+    calculateForwardPosition(
+      actor.getPos(),
+      actor.getOrientation(),
+    ),
+    actor,
+  );
 }
 
 function commandHandler(commandStr, actor) {
