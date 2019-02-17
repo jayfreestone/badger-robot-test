09f1034299fdbb33bd04919ce5f110568fa2dc5f
Jay Freestone <mail@jayfreestone.com>
2019-02-17 23:32:12 +0000

    refactor(commandHandler): Clean up, add docs and missing undos.

--- src/commandHandler/index.js
+++ src/commandHandler/index.js
@@ -2,17 +2,47 @@ const { LEFT, RIGHT } = require('./../consts/directions');
 const calculateOrientation = require('./../utils/calculateOrientation');
 const calculateForwardPosition = require('./../utils/calculateForwardPosition');
 
+/**
+ * @typedef {Object} actorType
+ * @property {Function} setOrientation
+ * @property {Function} getOrientation
+ * @property {Function} getPos
+ * @property {Function} moveTo
+ */
+
+/**
+ * @typedef {Object} commandType
+ * @property {Function} execute
+ * @property {Function} undo
+ */
+
+/**
+ * Creates a new rotation command to turn an actor in place.
+ * @param {{ dir: string }} options Contains the target direction, e.g. 'left'
+ * @param {actorType} actor
+ * @returns {commandType}
+ */
 function createRotateCommand({ dir }, actor) {
+  const oldOrientation = actor.getOrientation();
   return {
     execute() {
       actor.setOrientation(
         calculateOrientation(actor.getOrientation(), dir),
       );
     },
+    undo() {
+      actor.setOrientation(oldOrientation);
+    },
   };
 }
 
-function createMoveCommand(newPos, actor) {
+/**
+ * Create a new move command that updates the x/y of an actor.
+ * @param {{ pos: Array<number> }} newPos The new target position.
+ * @param {actorType} actor
+ * @returns {commandType}
+ */
+function createMoveCommand({ pos: newPos }, actor) {
   const oldPos = actor.getPos();
   return {
     execute() {
@@ -24,16 +54,31 @@ function createMoveCommand(newPos, actor) {
   };
 }
 
+/**
+ * Creates a 'move forward' command that advances an actor forward based on
+ * their current orientation.
+ * @param {actorType} actor
+ * @returns {commandType}
+ */
 function createMoveForwardCommand(actor) {
+  // Just returns a generic move command with values pre-calculated
   return createMoveCommand(
-    calculateForwardPosition(
-      actor.getPos(),
-      actor.getOrientation(),
-    ),
+    {
+      pos: calculateForwardPosition(
+        actor.getPos(),
+        actor.getOrientation(),
+      ),
+    },
     actor,
   );
 }
 
+/**
+ * Takes a command string, e.g. 'F' and translates it into a command object
+ * in the style of the command pattern.
+ * @param {string} commandStr
+ * @param {actorType} actor
+ */
 function commandHandler(commandStr, actor) {
   switch (commandStr) {
     case LEFT:
