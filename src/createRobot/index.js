13cfe133568943cb262b760e3097e77ae82bfe82
Jay Freestone <mail@jayfreestone.com>
2019-02-17 20:56:44 +0000

    feat(createRobot): Add working robot factory.

--- src/createRobot/index.js
+++ src/createRobot/index.js
@@ -1,5 +1,37 @@
+/**
+ * @typedef {Object} robotInstance
+ * @property {getPos} orientation
+ * @property {Array<Number>} pos
+ * @property {Array<String>} instructions
+ */
+
+/**
+ * Robot instance factory.
+ * @param {{ pos: [number, number], orientation: string }} inputObj
+ */
 function createRobot({ pos: initialPos, orientation: initialOrientation }) {
+  let pos = initialPos;
+  let orientation = initialOrientation;
+
   return {
+    /**
+     * @returns string;
+     */
+    getOrientation() {
+      return orientation;
+    },
+    setOrientation(newOrientation) {
+      orientation = newOrientation;
+    },
+    /**
+     * @returns string;
+     */
+    getPos() {
+      return pos;
+    },
+    moveTo(x, y) {
+      pos = [x, y];
+    },
   };
 }
 
