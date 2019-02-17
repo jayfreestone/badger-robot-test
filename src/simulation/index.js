073633c9324faead5bba46b115ade1e8c3a86978
Jay Freestone <mail@jayfreestone.com>
2019-02-17 23:08:35 +0000

    feat(simulation): Hook up levels and robots with output.

--- src/simulation/index.js
+++ src/simulation/index.js
@@ -1,10 +1,27 @@
 const parseInput = require('./../utils/parseInput');
+const outputToStr = require('./../utils/outputToStr');
 const createLevel = require('./../createLevel');
+const createRobot = require('./../createRobot');
 
 function simulation(input) {
   const { grid: { width, height }, robots } = parseInput(input);
 
   const levelInst = createLevel({ width, height });
+
+  robots.forEach(({ pos, orientation, instructions }) => {
+    levelInst.addRobot(
+      createRobot({ pos, orientation }),
+      instructions,
+    );
+  });
+
+  const finalPositions = levelInst.getRobots().map(robot => ({
+    pos: robot.getPos(),
+    lost: robot.getLost(),
+    orientation: robot.getOrientation(),
+  }));
+
+  return outputToStr(finalPositions);
 }
 
 module.exports = simulation;
