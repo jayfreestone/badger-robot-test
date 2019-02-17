878341b95ffa2e094c60886a16598f121df60330
Jay Freestone <mail@jayfreestone.com>
2019-02-17 23:37:40 +0000

    docs(outputToStr): Add comment.

--- src/utils/outputToStr/index.js
+++ src/utils/outputToStr/index.js
@@ -1,3 +1,15 @@
+/**
+ * @typedef {Object} robotData
+ * @property {Array<number>} pos
+ * @property {boolean} lost
+ * @property {string} orientation
+ */
+
+/**
+ * Converts robot data to the desired output string defined in the spec.
+ * @param {Array<robotData>} input An array of robot data objects.
+ * @returns {string}
+ */
 function outputToStr(input) {
   return input
     .map(({ pos: [x, y], lost, orientation }) => (
