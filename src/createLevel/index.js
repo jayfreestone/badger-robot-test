3eb35c650376e9247e9cc0f4441e22f87e10f14e
Jay Freestone <mail@jayfreestone.com>
2019-02-17 22:13:55 +0000

    feat(createLevel): Run commands up until out of bounds.

--- src/createLevel/index.js
+++ src/createLevel/index.js
@@ -3,9 +3,23 @@ const commandHandler = require('./../commandHandler');
 function createLevel({ width, height }) {
   const robots = [];
 
+  function withinBounds([x, y]) {
+    if (x < 0 || y < 0 || x > width || y > height) return false;
+    return true;
+  }
+
   function runInstruction(robotInst, instruction) {
     const command = commandHandler(instruction, robotInst);
     command.execute();
+
+    const newPosition = robotInst.getPos();
+
+    // We are okay, continue
+    if (withinBounds(newPosition)) {
+      return false;
+    }
+
+    return true;
   }
 
   function addRobot(robotInst, instructions) {
