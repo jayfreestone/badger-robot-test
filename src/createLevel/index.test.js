e483bd13d4b2d0dc099af3742c45a4281ac4d44b
Jay Freestone <mail@jayfreestone.com>
2019-02-17 21:52:20 +0000

    tests(createLevel): Update test in response to commandHandler.

--- src/createLevel/index.test.js
+++ src/createLevel/index.test.js
@@ -3,7 +3,11 @@ const createLevel = require('./index');
 describe('create a level', () => {
   test('that can add robots', () => {
     const inst = createLevel({ width: 5, height: 5 });
-    inst.addRobot({}, ['L', 'R', 'F']);
+
+    expect(inst.getRobots()).toHaveLength(0);
+
+    inst.addRobot({}, []);
+
     expect(inst.getRobots()).toHaveLength(1);
   });
 });
