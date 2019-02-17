0fba84f3ae88c6a3c1aa158b5080857423ddea96
Jay Freestone <mail@jayfreestone.com>
2019-02-17 21:20:26 +0000

    feat(commandHandler): Basic pass of move command.

--- src/commandHandler/index.js
+++ src/commandHandler/index.js
@@ -1,5 +1,18 @@
-function commandHandler(commandStr, actor) {
+function createMoveForwardCommand(actor) {
+  return {
+    execute() {
+      actor.moveTo();
+    },
+  };
+}
 
+function commandHandler(commandStr, actor) {
+  switch (commandStr) {
+    case 'F':
+      return createMoveForwardCommand(actor);
+    default:
+      throw new Error('Command not recognised');
+  }
 }
 
 module.exports = commandHandler;
