0b1a8c63f3422f7911f04a64d15d6aea0188aac4
Jay Freestone <mail@jayfreestone.com>
2019-02-17 23:19:12 +0000

    docs(simulation): Add comments.

--- src/simulation/index.js
+++ src/simulation/index.js
@@ -3,11 +3,17 @@ const outputToStr = require('./../utils/outputToStr');
 const createLevel = require('./../createLevel');
 const createRobot = require('./../createRobot');
 
+/**
+ * Create a new 'martian robots' simulation.
+ * @param {string} input The input string defined within the spec.
+ * @returns {string} The processed output string.
+ */
 function simulation(input) {
   const { grid: { width, height }, robots } = parseInput(input);
 
   const levelInst = createLevel({ width, height });
 
+  // Add and simulate each robot on the map
   robots.forEach(({ pos, orientation, instructions }) => {
     levelInst.addRobot(
       createRobot({ pos, orientation }),
@@ -15,6 +21,7 @@ function simulation(input) {
     );
   });
 
+  // The final robot positions as an array
   const finalPositions = levelInst.getRobots().map(robot => ({
     pos: robot.getPos(),
     lost: robot.getLost(),
